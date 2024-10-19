e2ba0b03b4d1f8420da77caf1064b53a4b432589
Ohm-Rajpal <ohm_rajpal@berkeley.edu>
2024-10-19 02:40:53 -0700

    auth in progress, we finish it when i wake up im tired

---
+++ contexts/authContext/index.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState, useContext } from "react"; 
+import { auth } from "../../firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useContext } from "react";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+
+    const [currentUser, setCurrentUser] = useState(null);
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
+    
+    // use effect
+    useEffect (() => {
+        const unsubscribe = onAuthStateChanged(auth, initializeUser);
+        return unsubscribe;
+    }, []);
+    
+    // initialize user
+    async function initializeUser(user) {
+        if (user) {
+            setCurrentUser({ ...user }); // spread arg
+            setUserLoggedIn(true);
+        } else { // user has logged off
+            setCurrentUser(null);
+            setUserLoggedIn(false);
+        }
+        setLoading(false);
+    }
+
+    // expose value obj
+    const value = {
+        currentUser,
+        userLoggedIn,
+        loading
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
