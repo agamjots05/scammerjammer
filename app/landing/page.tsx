5675ba56772102e536420db52a4f023a642a4351
agamjots <email.agamjotsingh@gmail.com>
2024-10-18 22:00:47 -0700

    fixed structure

---
+++ app/landing/page.tsx
@@ -0,0 +1,6 @@
+export default function landing(){
+    return (
+        <h1>Landing</h1>
+    )
+    
+}
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -47,9 +47,6 @@ export const Nav = () => {
           variant={"ghost"}
           className={"ml-auto flex items-center gap-1.5"}
         >
-          <span>
-            <Github className={"size-4"} />
-          </span>
           <span>Star on GitHub</span>
         </Button>
         <Button
